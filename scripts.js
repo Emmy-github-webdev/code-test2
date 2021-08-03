706f6931bb8456a81ac0c085ff2efdb509ba86f6
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-08-03 19:30:19 +0100

    commit html setup

--- scripts.js
+++ scripts.js
@@ -1,20 +1,2 @@
-function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    numofApple = 0;
-    numoforanges = 0;
 
-    for(let i = 0; i < apples.length; i++){
-        if(a + apples[i] >= s && a + apples[i] <= t){
-            numofApple++;
-        }
-    }
-
-    for(let j = 0; j < oranges.length; j++){
-        if(b + oranges[j] >= s && b + oranges[j] <= t){
-            numoforanges++;
-        }
-    }
-
-    console.log(numofApple);
-    console.log(oranges);
-};
 
